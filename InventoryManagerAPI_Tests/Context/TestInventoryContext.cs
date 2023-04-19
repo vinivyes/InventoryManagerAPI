af5211e55de9991855cddb561191a1490e207a4a
Vinicius de Morais <viniciusmorais352@gmail.com>
2023-04-18 19:49:08 -0600

    **InventoryManagerAPI_Tests** -Adding initial Unit Test setup. -Initial draft of UserControllerTests with In-Memory DB. -Test specific context to workaround In-Memory array limitations.
    ---------Context---------
    InventoryContext.cs:
    -Renamed File to match class name.

---
+++ InventoryManagerAPI_Tests/Context/TestInventoryContext.cs
@@ -0,0 +1,36 @@
+﻿using InventoryManagerAPI.Context;
+using InventoryManagerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Newtonsoft.Json;
+
+public class TestInventoryContext : InventoryContext
+{
+    public TestInventoryContext(DbContextOptions<InventoryContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Role>(ConfigureRole);
+    }
+
+    //Allow the use of string arrays in the memory database for testing purposes
+    private void ConfigureRole(EntityTypeBuilder<Role> builder)
+    {
+        builder.Property(r => r.allowedActions)
+            .HasConversion(
+                v => JsonConvert.SerializeObject(v),
+                v => JsonConvert.DeserializeObject<string[]>(v)
+            );
+
+        builder.Property(r => r.notAllowedActions)
+            .HasConversion(
+                v => JsonConvert.SerializeObject(v),
+                v => JsonConvert.DeserializeObject<string[]>(v)
+            );
+    }
+}
---
+++ InventoryManagerAPI_Tests/ControllerTests/UserControllerTests.cs
@@ -0,0 +1,91 @@
+﻿using InventoryManagerAPI.Context;
+using InventoryManagerAPI.Controllers;
+using InventoryManagerAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagerAPI_Tests.ControllerTests
+{
+    public class UserControllerTests
+    {
+        private TestInventoryContext GetTestDbContext()
+        {
+            //Creates a new DB Context for testing, uses a GUID to create a unique DB name for every test.
+            var options = new DbContextOptionsBuilder<InventoryContext>()
+                                        .UseInMemoryDatabase(databaseName: $"InventoryManager-{Guid.NewGuid()}")
+                                        .Options;
+
+            var dbContext = new TestInventoryContext(options);
+
+            //Creates an Admin User
+            User admin = new User { id = 1, first_name = "Admin", last_name = "", email = "admin@inventorym.com", password = BCrypt.Net.BCrypt.HashPassword("Password123!@#"), passwordDate = DateTime.UtcNow, roles = new List<Role>() };
+            Role adminRole = new Role { id = 1, name = "Administrator", isActive = true, allowedActions = new string[] { "*" } };
+
+            //Saves User and Role to DB
+            dbContext.Users.Add(admin);
+            dbContext.Roles.Add(adminRole);
+
+            //Creates a RoleUser to link the User and Role
+            admin.roles.Add(adminRole);
+
+            //Saves the changes to the DB
+            dbContext.SaveChanges();
+
+            return dbContext;
+        }
+
+        [Fact]
+        public void GetAll_ReturnsListOfUsers()
+        {
+            // Arrange
+            var dbContext = GetTestDbContext();
+            var controller = new UserController(dbContext);
+
+            // Act
+            var result = controller.GetAll();
+
+            // Assert
+            var objectResult = Assert.IsType<ActionResult<List<object>>>(result);
+            var users = Assert.IsAssignableFrom<List<object>>(objectResult.Value);
+            Assert.Single(users);
+        }
+
+        [Fact]
+        public void GetById_ReturnsUser_WhenIdExists()
+        {
+            // Arrange
+            var dbContext = GetTestDbContext();
+            var controller = new UserController(dbContext);
+            int existingId = 1;
+
+            // Act
+            var result = controller.GetById(existingId);
+
+            // Assert
+            var objectResult = Assert.IsType<ActionResult<object>>(result);
+            Assert.NotNull(objectResult.Value);
+        }
+
+        [Fact]
+        public void GetById_ReturnsNotFound_WhenIdDoesNotExist()
+        {
+            // Arrange
+            var dbContext = GetTestDbContext();
+            var controller = new UserController(dbContext);
+            int nonExistingId = 100;
+
+            // Act
+            var result = controller.GetById(nonExistingId);
+
+            // Assert
+            Assert.IsType<NotFoundObjectResult>(result.Result);
+        }
+
+        // Add more tests for other UserController methods
+    }
+}
